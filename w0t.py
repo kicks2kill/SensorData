c4aa262d24510bf78864af61fe8ede69df52cfce
Anonxlm <32618202+kicks2kill@users.noreply.github.com>
2018-03-13 08:45:35 -0400

    I've updated my w0t.py to be able to iterate through a list to gather columns from csv

--- w0t.py
+++ w0t.py
@@ -1,22 +1,81 @@
 import csv
+import random
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+#I generated constants to make it easier to access the columns within the spread sheet
+ACCELEROMETER_X = 0
+LIGHT  = 13
+GRAVITY_Y  = 5
+LINEAR_ACCEL_X =  7
+LINEAR_ACCEL_Y = 8
+TIME = 31
+SOUND = 21
+LATITUDE = 22
+LONGITUDE = 23
+PRESSURE = 20
 
-df = pd.read_csv("androsense.csv",skiprows=1, sep=";")
-#df.reindex(axis="columns",fill_value='|)
+#read from the csv file and wrap it in pandas
+df = pd.read_csv("androsense.csv.csv",skiprows=1, sep=";")
 
-print(df)
+#iterate through each column, for that item within range(len) append the index
+#to the list, then return
+def column(arr,lst):
+    ret = []
+    for i in range(0, len(arr)):
+        ret.append(arr[i][lst])
+    return ret
 
+   #generate our columns for use 
+GravCol = column(df.values,ACCELEROMETER_X)
+LightCol = column(df.values,LIGHT)
+TimeCol = column(df.values,TIME)
+LinearX = column(df.values,LINEAR_ACCEL_X)
+AccelX = column(df.values,ACCELEROMETER_X)
+LinearY = column(df.values,LINEAR_ACCEL_Y)
+Sound = column(df.values,SOUND)
+Latitude = column(df.values,LATITUDE)
+Longitude = column(df.values,LONGITUDE)
+Pressure = column(df.values,PRESSURE)
+
+#as long as our gravity is not 0, display the linearY 
+if(GravCol != 0):
+    plt.plot(GravCol)
+    plt.plot(LinearY)
+    plt.xlabel('GravCol')
+    plt.ylabel('LinearY')
+    plt.show()
+    print(plt)
+
+
+##As long as our light is 1 or greater, we know that the phone was not within a pocket.
+LightCol.sort()
+if(LightCol.count(1)):
+    plt.plot(LightCol)
+    plt.plot(AccelX)
+    
+# plt.plot(Sound)
+# plt.plot(Latitude)
+#plt.plot(Longitude)
+# plt.plot(Pressure)
+
+#plt.plot(TimeCol) 
+#plt.plot(LinearX)
+
+
+
+
+
+#shape up the rows/columns for ease of viewing(of the csv)
 row = df.shape[0] 
 col = df.shape[1]
-#Dataframe is df, it holds the variable reference as a list
-#shape represents the dimensonality of the dataframe
-plt.plot(df.iloc[:,col-2]) 
-#plot the graph out
-plt.ylabel('yuh')
-plt.xlabel('idk')
-out_png = 'time2.png'
+out_png = 'Yup.png'
 #export as a png
 plt.savefig(out_png, dpi=150)
 #save the pyplot as a png
+
+
+
+
+
+
